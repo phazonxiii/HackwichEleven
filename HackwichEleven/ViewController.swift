883a89462852a0a1ce1a2876151044b21a1d6703
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-04-04 13:09:33 -1000

    Completed Part 7

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -17,12 +17,14 @@ class ViewController: UIViewController {
     
     let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
     
-    let regionRadius: CLLocationDistance = 1000
+    let regionRadius: CLLocationDistance = 10000
     
     override func viewDidLoad() {
         super.viewDidLoad()
         centerMapOnLocation(location: initialLocation)
-        // Do any additional setup after loading the view.
+
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
     }
 
     func centerMapOnLocation(location: CLLocation)
